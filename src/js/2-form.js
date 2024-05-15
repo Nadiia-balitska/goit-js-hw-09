94ee85a11b356d5cbbc2bd5f9a11ac9fd3d3d64f
Nadiia Balitska <161743763+Nadiia-balitska@users.noreply.github.com>
2024-05-15 20:12:28 +0300

    task 2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,14 +1,43 @@
 const formEl = document.querySelector('.feedback-form');
 const SAVE_KEY = "feedback-form-state";
-const formDate = { email: "", message: "" };
-//!==================================================================
-function saveInData(key, value) {
-    const json = JSON.stringify(value);
-    localStorage.setItem(key, json);
+const formData = { email: "", message: "" };
 
+//!==================================================================
+
+formEl.addEventListener('input', e => {
+    const value = e.target.value.trim();
+    console.log(value);
+    formData[e.target.name] = value;
+    saveInStorage(SAVE_KEY, formData);
+
+
+});
+
+
+formEl.addEventListener('submit', e => {
+    e.preventDefault();
+    const email = e.currentTarget.elements.email.value.trim();
+    const message = e.currentTarget.elements.message.value.trim();
+
+    if (!email|| !message) {
+        return alert('email or message is empty');
+    }
+        
+        formData[e.target.name] = '';
+        formEl.reset();
+})
+
+//!==================================================================
+function saveInStorage(key, value) {
+    try {
+        const json = JSON.stringify(value);
+        localStorage.setItem(key, json);
+    } catch (error) {
+        console.log(error.message);
+    }
 }
 
-function loadFromData(key) {
+function loadFromStorage(key) {
     const data = localStorage.getItem(key);
     try {
         const result = JSON.parse(data);
@@ -18,12 +47,20 @@ function loadFromData(key) {
     }
 }
 //!==================================================================
+function init() {
+    const data = loadFromStorage(SAVE_KEY) || {};
+    formEl.elements.email.value = data.email || '';
+    formEl.elements.message.value = data.message || '';
+
+}
+init()
+
+//!==================================================================
 
 
-formEl.addEventListener('input', e => {
-    const value = e.target.value.trim();
-    formData[e.target.name] = value;
-    saveInData(SAVE_KEY, formDate)
-});
+
+
+
+
 
 
