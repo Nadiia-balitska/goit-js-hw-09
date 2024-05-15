4705f3de0c39fd9681115fa85e3b44806b917b57
Nadiia Balitska <161743763+Nadiia-balitska@users.noreply.github.com>
2024-05-15 16:57:55 +0300

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,18 +1,29 @@
 const formEl = document.querySelector('.feedback-form');
-const STORAGE_KEY = "feedback-form-state";
+const SAVE_KEY = "feedback-form-state";
 const formDate = { email: "", message: "" };
+//!==================================================================
+function saveInData(key, value) {
+    const json = JSON.stringify(value);
+    localStorage.setItem(key, json);
 
-function saveFromData(key, value) {
-    
 }
 
 function loadFromData(key) {
-    
+    const data = localStorage.getItem(key);
+    try {
+        const result = JSON.parse(data);
+        return result;
+    } catch (error) {
+        return data;
+    }
 }
+//!==================================================================
 
 
 formEl.addEventListener('input', e => {
     const value = e.target.value.trim();
     formData[e.target.name] = value;
+    saveInData(SAVE_KEY, formDate)
+});
 
-})
+
